bf56d598cd5c9e51e3052edf15861c08e7c51146
CharlesSBL <77samaritan77@proton.me>
2022-11-07 17:15:28 -0500

    up06.2

--- 05_Sidebar/script.js
+++ 05_Sidebar/script.js
@@ -1,6 +1,8 @@
 const sidebar = document.querySelector('.sidebar');
 const btn = document.querySelectorAll('button');
 
-btn.forEach(elem => {elem.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar-active')
-})})
+btn.forEach(elem => {
+    elem.addEventListener('click', () => {
+        sidebar.classList.toggle('sidebar-active');
+    })
+})
